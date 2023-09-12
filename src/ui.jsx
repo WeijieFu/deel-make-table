00ae72bc4c3ed113d86e3ab4cee0aac09a9c802b
WeijieFu <20438157+WeijieFu@users.noreply.github.com>
2023-09-12 13:25:15 +0100

    add instructions

--- src/ui.jsx
+++ src/ui.jsx
@@ -9,11 +9,13 @@ import {
   Stack,
   VerticalSpace,
   Inline,
+  Link,
+  Modal,
 } from "@create-figma-plugin/ui"
 import { emit } from "@create-figma-plugin/utilities"
 import { h } from "preact"
 import { useCallback, useEffect, useState } from "preact/hooks"
-
+import styles from "./styles.css"
 import { _Column } from "./components/_Column"
 
 function Plugin() {
@@ -56,6 +58,13 @@ function Plugin() {
   // useEffect(() => {
   //   console.log(columns)
   // }, [columns])
+  const [isModalOpen, setIsModalOpen] = useState(true)
+  const handleModalOpenClick = () => {
+    setIsModalOpen(true)
+  }
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+  }
   return (
     <Container space='medium'>
       <VerticalSpace space='medium' />
@@ -115,9 +124,61 @@ function Plugin() {
           >
             Make This Table
           </Button>
+          <Link
+            align='center'
+            fullWidth
+            onClick={handleModalOpenClick}
+          >
+            How to use this plugin?
+          </Link>
         </Stack>
       </Stack>
       <VerticalSpace space='medium' />
+
+      <Modal
+        open={isModalOpen}
+        onOverlayClick={handleModalClose}
+        onEscapeKeyDown={handleModalClose}
+      >
+        <div class={styles.modal}>
+          <div>
+            This plugin helps designers to scaffold basic structure of reusable
+            Data Grid.
+          </div>
+
+          <div>
+            <div>
+              <strong>How to use this plugin</strong>
+            </div>
+            <p>
+              1. Create an empty <strong>Section</strong> in Figma where you
+              want to put the components .
+            </p>
+            <p>2. Enter the name of the table, e.g. "People".</p>
+            <p>
+              3. Check if your table has Batch Actions, Row Actions or Double
+              Line Header.
+            </p>
+            <p>
+              4. Add the column names and tpyes. You can Add New, Delete or Move
+              Up/Down columns. <br />
+              <br />
+              Use "|" for line break in Double Line Header, e.g. "Contract|Start
+              Date"
+            </p>
+            <p>5. Select the Section, and click "Make This Table" button.</p>
+            <p>6. It can take a few seconds to import the components.</p>
+          </div>
+
+          <div>
+            <strong>Contact</strong>
+            <p>
+              If you have any feedback or suggestions, please contact @Weijie Fu
+              on Slack.
+            </p>
+          </div>
+        </div>
+      </Modal>
     </Container>
   )
 }
